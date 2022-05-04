fdcbd18851113baae3668714b2ffff29ffe95bde
Luiz Felipe Stangarlin <luizfelipestang@gmail.com>
2022-05-04 09:38:22 -0500

    Update android-env.sh

--- tools/android-env.sh
+++ tools/android-env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export ANDROID_HOME=/opt/android-sdk-linux
+export ANDROID_HOME=/opt/android-sdk
 export ANDROID_SDK_ROOT=${ANDROID_HOME}
 export ANDROID_SDK_HOME=${ANDROID_HOME}
 export ANDROID_SDK=${ANDROID_HOME}
